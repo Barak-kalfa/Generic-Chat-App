8d8530a2c80c23901036b10f0ed44e8d57d3aa7f
Barak Kalfa <barak.kalfa@gmail.com>
2023-05-28 13:55:16 +0300

    order by time and menu fullnames

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {
   setDoc,
   addDoc,
   updateDoc,
+  orderBy,
   Timestamp,
   onSnapshot,
   query,
@@ -24,15 +25,18 @@ const newGroupLink = document.querySelector("#new-group-link");
 const inviteLink = document.querySelector("#invite-link");
 const newChatInput = document.querySelector("#new-chat-input");
 const newChatButton = document.querySelector("#new-chat-button");
+const usersHeader = document.querySelector(".container1");
 
 const usersRef = collection(db, "users");
 const chatsRef = collection(db, "chats");
 const msgRef = collection(db, "messages");
+
 export let chatsSnapshot;
 let currentChatId;
 let currentUser = localStorage.getItem("uid")
   ? await setCurrentUser(localStorage.getItem("uid"))
   : undefined;
+
 console.log(currentUser);
 
 export async function setCurrentUser(uid) {
@@ -61,6 +65,22 @@ export async function createUser(name, email, uid) {
   }
 }
 
+async function getUsers(field, operator, value){
+  console.log(field, operator, value);
+  const users = [];
+  // field = "email";
+  //  operator = "==";
+  // value = "b@b.com";
+  const q =  query(usersRef, where(field, operator, value));
+  const querySnapshot = await getDocs(q);
+  await querySnapshot.forEach((user) => {
+    users.push(user);
+  })
+return users;
+}
+
+
+
 function handleMenuButton(input, callback) {
   switch (callback) {
     case "startNewChat":
@@ -80,7 +100,17 @@ function handleMenuButton(input, callback) {
 async function startNewChat(email) {
   //ADD EMAIL ALREADY IN CHATS LIST CHECK
   try {
-    const chat = { name: "", users: [currentUser.email, email] };
+    const chat = {
+      name: "",
+      users: [currentUser.email, email],
+      lastMessage: {
+        chatId: null,
+        time: null,
+        text: null,
+        userEmail: null,
+        userName: null,
+      },
+    };
     const docRef = await addDoc(collection(db, "chats"), chat);
   } catch (err) {
     console.log(err);
@@ -89,7 +119,13 @@ async function startNewChat(email) {
 
 async function startNewGroup(name) {
   try {
-    const chat = { name: name, users: [currentUser.email] };
+    const chat = { name: name, users: [currentUser.email], lastMessage: {
+      chatId: null,
+      time: null,
+      text: null,
+      userEmail: null,
+      userName: null,
+    } };
     const docRef = await addDoc(collection(db, "chats"), chat);
   } catch (err) {
     console.log(err);
@@ -106,20 +142,22 @@ async function inviteToGroup(email) {
 
 async function getChats(currentUserEmail) {
   if (chatsSnapshot) chatsSnapshot(); //detaches last snapshot
-  const q = query(chatsRef, where("users", "array-contains", currentUserEmail));
+  const q = query(chatsRef, where("users", "array-contains", currentUserEmail), orderBy("lastMessage.time", "asc"));
   chatsSnapshot = onSnapshot(q, (snapshot) => {
-    snapshot.docChanges().forEach((change) => {
+    snapshot.docChanges().forEach(async(change) => {
       if (change.type === "added") {
         console.log("New Chat: ", change.doc.data());
         const chatBox = document.createElement("a");
         const chat = change.doc.data();
-        if (chat.name === "") {
-          chat.name = chat.users.filter((chat) => chat !== currentUser.email);
+        if (chat.name === "") { //chat.name != "" means its a group
+          const otherUserEmail = chat.users.filter((chat) => chat !== currentUser.email);
+          const otherUser = await getUsers("email", "==", otherUserEmail[0]);
+          chat.name = otherUser[0].data().fullName;
         }
         chatBox.innerText = chat.name;
         chatBox.addEventListener("click", () => {
-          getMessages(change.doc.id);
           setCurrentChat(change.doc.id);
+          getMessages(currentChatId);
         });
         chatsBar.appendChild(chatBox);
       }
@@ -133,12 +171,25 @@ async function getChats(currentUserEmail) {
   });
 }
 
-function setCurrentChat(chatId) {
+export let membersSnapshot;
+async function setCurrentChat(chatId) {
+  if (currentChatId) membersSnapshot();
   currentChatId = chatId;
+  const chat = await getDoc(doc(db, "chats", chatId));
+  const q = query(usersRef, where("email", "in", chat.data().users));
+  membersSnapshot = await getDocs(q);
+  membersSnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    const groupMember = document.createElement("img");
+    groupMember.src = "https://mdbcdn.b-cdn.net/img/new/avatars/2.webp";
+    groupMember.classNames = "msgimg";
+    usersHeader.appendChild(groupMember);
+  });
 }
 
 async function sendMsg() {
-  const time = new Date().toDateString();
+  const date = new Date();
+  const time = Timestamp.fromDate(date);
   const message = {
     chatId: currentChatId,
     time: time,
@@ -147,6 +198,8 @@ async function sendMsg() {
     userName: currentUser.fullName,
   };
   await setDoc(doc(collection(db, "messages")), message);
+  const chatRef = await doc(db, "chats", currentChatId)
+  await updateDoc(chatRef, { lastMessage: message });
 }
 
 function renderMessage(message) {
@@ -161,7 +214,7 @@ function renderMessage(message) {
           <p class="multi-msg">
           ${message.text}
           </p>
-          <span class="time">${message.time}</span>
+          <span class="time">${new Date(message.time.toDate())}</span>
           </div>
           </div>`;
     msgPage.appendChild(msg);
@@ -185,14 +238,19 @@ function renderMessage(message) {
 }
 
 export let messagesSnapshot;
+
 async function getMessages(chatId) {
   if (messagesSnapshot) messagesSnapshot();
   msgPage.innerHTML = "";
   try {
-    const q = query(msgRef, where("chatId", "==", chatId));
+    const q = query(
+      msgRef,
+      where("chatId", "==", chatId),
+      orderBy("time", "asc")
+    );
     messagesSnapshot = onSnapshot(q, (snapshot) => {
-      snapshot.forEach((message) => console.log(message));
       snapshot.docChanges().forEach((change) => {
+        console.log(change.time);
         if (change.type === "added") {
           console.log("New Message: ", change.doc.data());
           renderMessage(change.doc.data());
